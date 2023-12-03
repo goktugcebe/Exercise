9cc68b08d2daf6e87e4c01dc62e4814811d8fba7
goktu <goktugcebe@gmail.com>
2023-12-03 03:28:37 +0100

    first commit

---
+++ write-read-file.py
@@ -0,0 +1,33 @@
+from random import randint as rnd
+
+memReg='members.txt'
+exReg='inactive.txt'
+fee=('yes','no')
+
+def genFiles(current,old):
+    with open(current,'w+') as writefile:
+        writefile.write('Membership No  Date  Joined  Active  \n')
+        data="{:^13}  {:<11}  {:<6}\n"
+        
+        for rowno in range(20):
+            date = str(rnd(2015,2020))+'-'+str(rnd(1,12))+'-'+str(rnd(1,25))
+            writefile.write(data.format(rnd(10000,99999),date,fee[rnd(0,1)]))
+
+    with open(old,'w+') as writefile:
+        writefile.write('Membership No  Date  Joined  Active  \n')
+        data="{:^13}  {:<11}  {:<6}\n"
+        for rowno in range(3):
+            date = str(rnd(2015,2020))+'-'+str(rnd(1,12))+'-'+str(rnd(1,25))
+            writefile.write(data.format(rnd(10000,99999),date,fee[1]))
+
+genFiles(memReg,exReg)
+
+
+
+with open(memReg,'r') as readFile:
+    print("Active Members: \n\n")
+    print(readFile.read())
+    
+with open(exReg,'r') as readFile:
+    print("Inactive Members: \n\n")
+    print(readFile.read())
